51c6d113f22acbf5a29bf243e550a5f7fbc0b6e1
littlehome <littlehome@littlehomeui-MacBookAir.local>
2023-12-15 17:46:44 +0900

    wip

--- src/open_investing/client_adapter/market_event.py
+++ src/open_investing/client_adapter/market_event.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 from typing import Dict
-from open_investing.market_event.market_event import MarketEventSpec
+from open_investing.market_event.market_event import MarketEventSpec, IMarketEventSource
 
 
 class MarketEventSourceRegistry:
-    event_source_classes: Dict{MarketEvent, MarketEventSource} = {}
+    event_source_classes: Dict[MarketEventSpec, IMarketEventSource] = {}
 
     @classmethod
-    def register_class(cls, market_event: MarketEvent, target_cls):
-        if market_event in cls.event_source_classes:
-            raise Exception(f"MarketEvent {market_event} is already registered")
+    def register_class(cls, market_event_spec: MarketEventSpec, target_cls):
+        if market_event_spec in cls.event_source_classes:
+            raise Exception(
+                f"MarketEventSpec {market_event_spec} is already registered"
+            )
 
-        cls.event_source_classes[market_event] = target_cls
+        cls.event_source_classes[market_event_spec] = target_cls
 
     @classmethod
     def initialize_startup_classes(cls):
-        for registered_cls in cls.startup_classes:
+        for registered_cls in cls.event_source_classes:
             obj = registered_cls()
             obj.init()
