c8cd00294999ba7496421604ecf34b0d33c6d6be
littlehome <littlehome@littlehomeui-MacBookAir.local>
2023-12-18 17:57:22 +0900

    f

--- src/open_investing/client_adapter/market_event.py
+++ src/open_investing/client_adapter/market_event.py
@@ -3,20 +3,21 @@ from typing import Dict
 from open_investing.market_event.market_event import MarketEventSpec, IMarketEventSource
 
 
-class MarketEventSourceRegistry:
-    event_source_classes: Dict[MarketEventSpec, IMarketEventSource] = {}
+class TaskSpecHandlerRegistry:
+    task_spec_handler_classes: Dict[TaskSpec, TaskSpecHandler] = {}
 
     @classmethod
-    def register_class(cls, market_event_spec: MarketEventSpec, target_cls):
-        if market_event_spec in cls.event_source_classes:
-            raise Exception(
-                f"MarketEventSpec {market_event_spec} is already registered"
-            )
+    def register_class(cls, task_spec_class: TaskSpec, target_cls):
+        if task_spec_class in cls.task_spec_handler_classes:
+            raise Exception(f"TaskSpec {task_spec_class} is already registered")
 
-        cls.event_source_classes[market_event_spec] = target_cls
+        cls.task_spec_handler_classes[task_spec_class] = target_cls
 
     @classmethod
-    def initialize_startup_classes(cls):
-        for registered_cls in cls.event_source_classes:
-            obj = registered_cls()
-            obj.init()
+    def create_instance(cls, task_spec: TaskSpec):
+        task_spec_cls = type(task_spec)
+
+        if task_spec_cls not in cls.task_spec_handler_classes:
+            raise Exception(f"TaskSpec {task_spec} is not registered")
+
+        task_spec_handler_cls = cls.task_spec_handler_classes[task_spec_cls]
